8771b3c665a4c49f97968df5674d4df966101126
Erico <ericoam.mussulin5@gmail.com>
2023-09-11 21:37:06 -0300

    Configurando entity

---
+++ Solution/FichaCadastroSln/FichaCadastro/Model/FichaCadastroContextDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace FichaCadastro.Model
+{
+    public class FichaCadastroContextDB : DbContext
+    {
+        public FichaCadastroContextDB(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
